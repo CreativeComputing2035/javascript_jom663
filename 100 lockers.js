2a4b1d2c8325c6eae57d90b8b991cc9bcb157a21
jom663 <moonnyungjo@Moonnyungs-MBP.fios-router.home>
2016-11-01 15:42:12 -0400

    javascript math problem
    I searched the math code from the internet to divide or multiply.
    I tried the locker problem but i still can’t solve

---
+++ 100 lockers.js
@@ -0,0 +1,5 @@
+ var myArray = [n = open]    
+    for (var i = 1; i <= 100; i++) { for (int n = i; n <= 100; n + = i){if (n = open){open = close}}{if (n = close) {close = open }}
+} 
+console.log(n = open );
+
---
+++ pythagorean.js
@@ -0,0 +1,5 @@
+function pythagorean(A,B){
+  return Math.sqrt(Math.pow(A, 2) + Math.pow(B, 2));
+}
+
+console.log(pythagorean(A, B));
